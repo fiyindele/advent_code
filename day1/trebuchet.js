dfd5ca1e473d6f83a56a8aa3a15e173965d3b9c4
fiyin Oladele <fiyin_oladele@yahoo.com>
2023-12-01 01:46:08 -0800

    Initial commit

---
+++ day1/trebuchet.js
@@ -0,0 +1,21 @@
+/*
+- prompt the user
+- save the entry
+- check how many lines?
+- for each line search through for the digits and save them in a temp array (digits)
+- then search trhough the array for the first and last digits entered
+- put both digits together to form one number calibration and save in the array (calibration)
+- now add all the calibration numbers in the array (calibration) to get the SUM. */
+
+// prompting the user for calibration entries
+let elfInput = prompt("Enter calibration entry: ");
+
+// this array saves all digits on a line entry temporarily
+let digits = [];
+
+// search through each character on the line for digits
+for (let i = 0; i < elfInput.length; i++) {
+  const value = array[i];
+  if (value >= 0 && value <= 9) {
+  }
+}
