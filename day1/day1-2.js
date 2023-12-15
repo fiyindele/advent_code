80763b44636f7f746c18fbba209c3a6d8c91018c
fiyin Oladele <fiyin_oladele@yahoo.com>
2023-12-15 15:31:08 -0800

    update

--- day1/day1-2.js
+++ day1/day1-2.js
@@ -1,4 +1,4 @@
-function sumCalibrationValuesWithSpelledOutDigits(calibrationDocument) {
+function sumIncludingSpelledOutDigits(calibrationDocument) {
   // Split the calibration document into lines
   const lines = calibrationDocument.split("\n");
 
@@ -6,9 +6,16 @@ function sumCalibrationValuesWithSpelledOutDigits(calibrationDocument) {
   let sum = 0;
 
   // Iterate through each line
+  /*
   for (const line of lines) {
     // Use a regular expression to match digits or spelled-out numbers
-    const matches = line.match(/\d+|[a-z]+/gi);
+    const matches = line.match(
+      /(?:[1-9]|one|two|three|four|five|six|seven|eight|nine)/gi
+    );
+    console.log(matches);
+    //const matches = line.match(/\d+|[a-z]+/gi);
+    //"/(?:[1-9]|one|two|three|four|five|six|seven|eight|nine)/i"
+    //"/\d+|(one|two|three|four|five|six|seven|eight|nine)+/gi"
 
     // Extract the real first and last elements
     const firstElement = matches[0];
@@ -16,6 +23,27 @@ function sumCalibrationValuesWithSpelledOutDigits(calibrationDocument) {
 
     // Convert the elements to numbers and add to the sum
     sum += parseInt(firstElement, 10) + parseInt(lastElement, 10);
+    console.log(
+      "firstElement: " +
+        parseInt(firstElement, 10) +
+        ", lastElement: " +
+        parseInt(lastElement, 10)
+    );
+  }
+  */
+  for (const line of lines) {
+    const digitsOnly = line.replace(/\D/g, "");
+
+    // Check if there are valid digits before trying to convert
+    if (digitsOnly.length > 0) {
+      const firstDigit = parseInt(digitsOnly.charAt(0), 10);
+      const lastDigit = parseInt(digitsOnly.charAt(digitsOnly.length - 1), 10);
+
+      // Check if conversion is successful before adding to the sum
+      if (!isNaN(firstDigit) && !isNaN(lastDigit)) {
+        sum += firstDigit + lastDigit;
+      }
+    }
   }
 
   return sum;
@@ -30,7 +58,7 @@ xtwone3four
 zoneight234
 7pqrstsixteen`;
 
-const resultWithSpelledOutDigits = sumCalibrationValuesWithSpelledOutDigits(
+const resultWithSpelledOutDigits = sumIncludingSpelledOutDigits(
   calibrationDocumentWithSpelledOutDigits
 );
-console.log(resultWithSpelledOutDigits); // Output: 281
+console.log("resultWithSpelledOutDigits: " + resultWithSpelledOutDigits); // Output: 281
